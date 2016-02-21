dc96e031cd248c400c88e206177a6538e01c32ea
wanying <18233568533@163.com>
2016-02-21 23:34:59 +0800

    完成基本功能

--- app/app.js
+++ app/app.js
@@ -8,5 +8,18 @@ angular.module('myApp', [
   'moviesCat.top',
 ]).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/theater'});
-}]);
+  $routeProvider.otherwise({redirectTo: '/in_theaters/1'});
+}])
+.controller("changeStatus",["$scope",'$location',function($scope,$location) {
+	$scope.location = $location;
+	$scope.$watch('location.path()',function(now) {
+		if (now.startsWith('/in_theaters')) {
+        $scope.type = 'in_theaters';
+       } else if (now.startsWith('/coming_soon')) {
+               $scope.type = 'coming_soon';
+       } else if (now.startsWith('/top250')) {
+         $scope.type = 'top250';
+       }
+		})
+
+	}]);
--- app/theater/controller.js
+++ app/theater/controller.js
@@ -3,19 +3,35 @@
 
 		var model = angular.module('moviesCat.theater',['ngRoute','movieCathttpServer']);
 		model.config(['$routeProvider',function($routeProvider) {
-			$routeProvider.when('/theater',{
+			$routeProvider.when('/:catagory/:page',{
 					controller: 'theater',
 					templateUrl: 'theater/view.html'
 				})
 			}]);
-		model.controller('theater',['$scope','$http','httpServer',function($scope, $http,httpServer) {
+		model.controller('theater',[
+			'$scope',
+			'$route',
+			'$routeParams',
+			'httpServer',
+			function($scope,$route,$routeParams,httpServer) {
 			$scope.loading = true;
-			httpServer.jsonp("http://api.douban.com/v2/movie/in_theaters",{},function(data){
-				$scope.title = data.title;
-				$scope.subjects = data.subjects;
-				$scope.loading = false;
-				$scope.$apply();
-				})
+			var count = 10;
+			var page = parseInt($routeParams.page);
+			var start = (page-1)*count;
+			$scope.currentPage = page;
+			httpServer.jsonp("http://api.douban.com/v2/movie/"+$routeParams.catagory,
+				{start:start,count:count},
+				function(data){
+					$scope.title = data.title;
+					$scope.total = data.total;
+					$scope.totalPage = Math.ceil($scope.total/count);
+					$scope.subjects = data.subjects;
+					$scope.loading = false;
+					$scope.$apply();
+				});
+			$scope.go = function(page) {
+				$route.updateParams({page : page});
+			}
 			}])
 
 	})(angular);
